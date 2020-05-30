472a1c5124c6b8c37d4990555972c9f81fbab651
fred <eneiford@gmail.com>
2020-05-30 02:45:41 -0500

    updated keymap

--- keyboards/dz60/keymaps/eneiford/keymap.c
+++ keyboards/dz60/keymaps/eneiford/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  /* _WIN LAYER
  *
  * ,-----------------------------------------------------------------------------------------.
- * | ESC |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  BKSPACE  |
+ * | ESC~|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  BKSPACE  |
  * | ----------------------------------------------------------------------------------------|
  * |  TAB  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  {  |  }  |  \   |  | 
  * | ----------------------------------------------------------------------------------------|
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------'
 */		
 	[_WIN] = LAYOUT_60_ansi_split(
-	    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, 
+	    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, 
 	    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
         LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
 	    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  /* _MAC LAYER
  *
  * ,-----------------------------------------------------------------------------------------.
- * | ESC |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  BKSPACE  |
+ * | ESC~|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  BKSPACE  |
  * | ----------------------------------------------------------------------------------------|
  * |  TAB  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  {  |  }  |  \   |  | 
  * | ----------------------------------------------------------------------------------------|
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------'
 */		
 	[_MAC] = LAYOUT_60_ansi_split(
-	    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, 
+	    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, 
 	    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
 	    LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
 	    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | ----------------------------------------------------------------------------------------|
  * |  CTRL  |  [  |  ]  |  {  |  }  |     | LEFT| DOWN|  UP |RIGHT| END | PGDN|    RETURN    |
  * | ----------------------------------------------------------------------------------------|
- * |     (     |     |     |     |     |     |     |     |  <  |  >  |     |       )         |
+ * |   SHIFT ( |     |     |     |     |     |     |     |  <  |  >  |     |   SHIFT  )      |
  * | ----------------------------------------------------------------------------------------|
- * |  CTRL |  ALT  | CMD  |   DEL    | _VIM  |      SPACE      |  ALT  | CMD  | CTRL | _RGB  |
+ * |  CTRL |  ALT  | CMD  | BACKSPACE| _VIM  |      SPACE      |  ALT  | CMD  | CTRL | _RGB  |
  * `-----------------------------------------------------------------------------------------'
 */		
 
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, 
 	    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_HOME, KC_PGUP, KC_NO, 
 	    KC_LCTL, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, KC_PGDN, KC_ENT, 
-	    KC_LPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_NO, KC_RPRN, 
-	    KC_LCTL, KC_LALT, KC_LGUI, KC_DEL, KC_TRNS, KC_SPC, KC_RALT, KC_RGUI, KC_RCTL, MO(3)
+	    KC_LSPO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_NO, KC_RSPC, 
+	    KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, KC_TRNS, KC_SPC, KC_RALT, KC_RGUI, KC_RCTL, MO(3)
 	),
  /* _RGB LAYER
  *
